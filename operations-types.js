4bd41f68e9a945e1f89813dcaf0cd935157f9d56
pearsongazelle-ui <pearsongazelle@gmail.com>
2025-10-30 09:50:49 +0200

    Add files via upload

---
+++ operations-types.js
@@ -0,0 +1,19 @@
+// TODO: Create a variable called number1 and store any number in it
+let number1 = 5;
+
+// TODO: Create a variable called number2 and store a different number in it
+let number2 = 10;
+
+// TODO: Convert number1 to a string and store it in a new variable
+let number1AsString = number1.toString();
+
+// TODO: Add number1 and number2 and store the result in a variable called sum
+let sum = number1 + number2;
+
+// TODO: Multiply number1 and number2 and store the result in a variable called product
+let product = number1 * number2;
+
+// TODO: Print all results to the console
+console.log("Number 1 as string: " + number1AsString);
+console.log("Sum of number1 and number2: " + sum);
+console.log("Product of number1 and number2: " + product);
